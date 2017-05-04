83cefd427c17a5d3f50306f3b1baa3887ee4cdae
Matthew Marchese <maffblaster@gentoo.org>
2017-05-04 16:41:56 -0700

    Start work on displaying uptime.

--- sheen/sheen.py
+++ sheen/sheen.py
@@ -29,7 +29,9 @@ import socket # Get IP addresses
 import getpass # Get (current) username
 import sys
 import logging
-from os import getenv
+
+import datetime
+from os import getenv # Specific user environment values
 
 from docopt import docopt
 
@@ -40,12 +42,32 @@ if sys.version_info < (3, 0):
     print(__name__ + " requires Python 3.0 and up. Exiting...\n")
     sys.exit(1)
 
+def get_uptime(system):
+    """Returns a human-readable string based on system"""
+
+    if system == 'Linux':
+        with open('/proc/uptime', 'r') as f:  # First number is uptime in seconds
+            output = f.readline().split(' ')  # Only read the first line; file should never be bigger
+            seconds = round(float(output[0]))
+            uptime = datetime.datetime(second=seconds)
+            print(str(seconds) + 's')
+            minuets = seconds / 60
+            hours = minuets / 60
+            days = hours / 24
+            years = days / 356
+
+            uptime = str(years) + 'y ' + str(days) + 'd ' + str(hours) + 'h ' + str(minuets) + 'm '
+    else:
+        uptime = 'Unknown...'
+
+    return uptime
+
 def show_platform():
     logging.debug('')
     print(getpass.getuser() + '@' + platform.node())
     print('OS: ' + str(platform.system()))
-    print('Kernel: ' + platform.machine() + ' ' + platform.release())
-    #print('Uptime: ')
+    print('Kernel: ' + platform.machine() + ' ' + platform.system() + ' ' + platform.release())
+    print('Uptime: ' + get_uptime(platform.system()))
     # print('Packages: ')
     print('Shell: ' + getenv('SHELL'))
     print('IPv4: ' + socket.gethostbyname(socket.gethostname()))
